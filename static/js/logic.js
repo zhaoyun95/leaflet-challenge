a23fbac48a9a24d38e126776bd0914d953007b2e
James Ye <998065+zhaoyun95@users.noreply.github.com>
2020-06-26 14:31:14 -0700

    first complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,49 +1,34 @@
-// // base url to get GeoJSON data from usgs.gov
-// var baseUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
-
-// // parameters for the query
-// var starttime = "&starttime=2020-05-01"
-// var endtime = "&endtime=2020-5-31"
-// var minmagnitude = "&minmagnitude=4"
-
-// // complete query url
-// var url = baseUrl + starttime + endtime + minmagnitude;
-
-// check Day 1 Activity-10 for GeioData example
-
-
-// -------------- sample map with marker -------------------
-// // var map = L.map('map').setView([51.505, -0.09], 13);
-// var map = L.map('map').setView([51.505, -0.09], 5);;
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
-
-// earthquake data for the last 7 days
-// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// past day
+// earthquake data for last day;
 // var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 // earthquake data for last hour;
-// fast loading for debug
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+//var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 
-console.log("url");
+// earthquake data for the last 7 days
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+console.log(url);
 
-// Perform a GET request to the url
-// GeoJSON example https://leafletjs.com/examples/geojson/
-d3.json(url).then(function(data){
-    console.log(data);
-    createFeatures(data.features);
+// plate boundaries
+var boundariesFile = "static/data/PB2002_plates.json";
+console.log(boundariesFile);
+
+// d3 Promises
+var promises = [];
+promises.push(d3.json(url));
+promises.push(d3.json(boundariesFile));
+
+// d3 Promises, reject if one of the promises failed
+Promise.all(promises).then(function (values){
+    console.log(values);
+    var earthquakeData = values[0];
+    var boundariesData = values[1];
+    createFeatures(earthquakeData, boundariesData);
+}).catch(function(error) {
+    console.log(error);
 });
 
 
+// return a color for the Legend
 function getColor(d) {
     return d > 5  ? 'rgb(250,   0, 0)' :
            d > 4  ? 'rgb(250,  75, 0)' :
@@ -53,11 +38,10 @@ function getColor(d) {
                     'rgb(  0, 250, 0)';
 }
 
-function createFeatures(earthquakeData) {
+// create earthquake circles and fault line.
+function createFeatures(earthquakeData, boundariesData) {
 
-    
-
-    function onEachFeature(feature, layer) {
+    function createPopups(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>magnitude: " +
         feature.properties.mag + "</p>");
@@ -86,23 +70,65 @@ function createFeatures(earthquakeData) {
     // Create a GeoJSON layer containing the features array
     // Run the onEachFeature function once for each feature
     var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
+        onEachFeature: createPopups,
         pointToLayer: createCircleMarker
     });
 
-    createMap(earthquakes);
+    // tectonicplates (bonus)
+    // add the Fault Lines layer
+    // see Day 2 Activity 1 last example (logic4.js)
+    // see explaination of each json file 
+    // https://github.com/fraxen/tectonicplates/tree/master/original
+    var faultLines = L.geoJSON(boundariesData, {
+
+        style: function(feature) {
+            return {
+                color: "red",
+                fillColor: "blue",
+                fillOpacity: 0.1,
+
+                weight: 3
+            };
+        },
+
+        // Called on each feature
+        onEachFeature: function(feature, layer) {
+            layer.on({
+                mouseover: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 0.2
+                    });
+                },
+
+                mouseout: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                        fillOpacity: 0.1
+                    })
+                },
+
+                click: function(event) {
+                    myMap.fitBounds(event.target.getBounds());
+                }
+            })
+        }
+
+    });
+
+    createMap(earthquakes, faultLines);
 }
 
 
-
-function createMap(earthquakes) {
-    // Define streetmap and darkmap layers
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// create the map with base map layers, and earthquake circles, fault lines, overlay menu, and legend
+function createMap(earthquakes, faultLines) {
+    // Define 5 tileLayers with different styles
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "streets-v11",
         accessToken: API_KEY
     });
 
@@ -110,23 +136,46 @@ function createMap(earthquakes) {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
+        // id: "light-v10",
         accessToken: API_KEY
     });
 
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-streets-v11",
+        accessToken: API_KEY
+    });
 
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
 
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v11",
+        accessToken: API_KEY
+    });
 
     // Define a baseMaps object to hold our base layers
     // this is submenu with two choices
     var baseMaps = {
-        "Street Map": streetmap,
-        "Dark Map": darkmap
+        "Satellite": satellite,
+        "Grayscale": grayscale,
+        "Outdoors": outdoors,
+        "Light": streetmap,
+        "Dark": darkmap
     };
 
     // Create overlay object to hold our overlay layer
     // this is another menu choice
     var overlayMaps = {
-        Earthquakes: earthquakes
+        "FaultLines": faultLines,
+        "Earthquakes": earthquakes
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -138,7 +187,7 @@ function createMap(earthquakes) {
         // stretmap: L.tileLayer layer
         // earthquakes: L.GeoJSON layer
         // these are the active layers
-        layers: [streetmap, earthquakes]
+        layers: [satellite, faultLines, earthquakes]
     });
 
     // Create a layer control
@@ -163,118 +212,15 @@ function createMap(earthquakes) {
         // need css in style.css to show the colored squares
         for (var i = 0; i < magnitudes.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
-                magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+                '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> <span>' +
+                magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '</span><br>' : '+</span>');
         }
 
         return div;
     };
-    
-      // Adding legend to the map
+
+    // Adding legend to the map
     legend.addTo(myMap);
-
-
-      // tectonicplates (bonus)
-      // add the Fault Lines layer
-      // see Day 2 Activity 1 last example (logic4.js)
-
-    // var platesFile = "static/data/PB2002_plates.json";
-    // d3.json(platesFile).then(function(platesData){
-    //     console.log("plates");
-    //     console.log(platesData);
-
-    //     L.geoJSON(platesData, {
-
-    //         style: function(feature) {
-    //             return {
-    //                 color: "yellow",
-    //                 fillColor: "purple",
-    //                 fillOpacity: 0.1,
-    //                 weight: 3
-    //             };
-    //         },
-
-    //         // Called on each feature
-    //         onEachFeature: function(feature, layer) {
-    //             layer.on({
-    //                 mouseover: function(event) {
-    //                     layer = event.target;
-    //                     layer.setStyle({
-    //                         fillOpacity: 0.2
-    //                     });
-    //                 },
-
-    //                 mouseout: function(event) {
-    //                     layer = event.target;
-    //                     layer.setStyle({
-    //                         fillOpacity: 0.1
-    //                     })
-    //                 },
-
-    //                 click: function(event) {
-    //                     myMap.fitBounds(event.target.getBounds());
-    //                 }
-    //             })
-    //         }
-
-    //     }).addTo(myMap);
-    // });
-
-    var stepsFile = "static/data/PB2002_steps.json";
-    d3.json(stepsFile).then(function(stepsData){
-        console.log("steps");
-        console.log(stepsData);
-    });
-
-    var boundariesFile = "static/data/PB2002_plates.json";
-    d3.json(boundariesFile).then(function(boundariesData){
-        console.log("boundaries");
-        console.log(boundariesData);
-        L.geoJSON(boundariesData, {
-
-            style: function(feature) {
-                return {
-                    color: "yellow",
-                    fillColor: "purple",
-                    fillOpacity: 0.1,
-
-                    weight: 3
-                };
-            },
-
-            // Called on each feature
-            onEachFeature: function(feature, layer) {
-                layer.on({
-                    mouseover: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: 0.2
-                        });
-                    },
-
-                    mouseout: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: 0.1
-                        })
-                    },
-
-                    click: function(event) {
-                        myMap.fitBounds(event.target.getBounds());
-                    }
-                })
-            }
-
-        }).addTo(myMap);
-        
-    });
-
-    var orogensFile = "static/data/PB2002_plates.json";
-    d3.json(orogensFile).then(function(orogensData){
-        console.log("orogens");
-        console.log(orogensData);
-    });
-   
 }
 
 
