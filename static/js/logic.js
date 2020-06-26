ccc6e147b9f27f668763cf1b6d72e7703b36bce7
James Ye <998065+zhaoyun95@users.noreply.github.com>
2020-06-26 16:45:38 -0700

    updated streetmap attribution

--- static/js/logic.js
+++ static/js/logic.js
@@ -124,7 +124,7 @@ function createFeatures(earthquakeData, boundariesData) {
 function createMap(earthquakes, faultLines) {
     // Define 5 tileLayers with different styles
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
